25c357bc2ea33690bf053205c3c8cea7f61860d1
unknown <eduardo.correap17@gmail.com>
2025-10-23 11:39:18 -0300

    +alterações

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ import math
 from pygame import Rect 
 
 GAME_ACTIVE = False
-
+GAME_WON = False
 WIDTH = 900
 HEIGHT = 500
-
 GRAVITY = 0.8
 JUMP_SPEED = -15
 MOVE_SPEED = 5
-ANIMATION_SPEED = 0
+ANIMATION_SPEED = 0.1
+PLATFORM_IMAGE = 'platform'
+ENEMY_SPEED = 1
+FLOOR_COLOR = (100, 50, 0)  
+BROTHER_POS = (450, HEIGHT - 85)
+
 
 player = Actor('player_sprite')
-player.pos = (-35, 0)
+player.pos = (0, -50)
 player.vy = 0
 player.on_ground = False
 player.facing_right = True
@@ -21,15 +25,24 @@ player.is_jumping = False
 player.animation_time = 0
 player.current_frame = 0
 
+brother = Actor('brother_idle')
+brother.pos = BROTHER_POS
+
+
+
 BUTTON_WIDTH = 200
 BUTTON_HEIGHT = 50
-
 START_COLOR = (50, 168, 82)    
 START_HOVER = (60, 198, 97)    
 EXIT_COLOR = (168, 50, 50)     
 EXIT_HOVER = (198, 60, 60)     
 MUSIC_COLOR = (238, 132, 0)    
 MUSIC_HOVER = (255, 165, 0)  
+INITIAL_ENEMIES = [
+    {'pos': (370, HEIGHT-190)},
+    {'pos': (670, HEIGHT-290)}
+]
+
 
 last_start_hover = False
 last_exit_hover = False
@@ -40,58 +53,135 @@ exit_hover = False
 MUSIC_BUTTON_RADIUS = 25
 music_playing = True
 music_hover = False
-
 music_button_pos = (WIDTH - 40, 40)
+
 start_button_pos = Rect((WIDTH/2 - BUTTON_WIDTH/2, HEIGHT/2 - 60), (BUTTON_WIDTH, BUTTON_HEIGHT))
 exit_button_pos = Rect((WIDTH/2 - BUTTON_WIDTH/2, HEIGHT/2 + 20), (BUTTON_WIDTH, BUTTON_HEIGHT))
 
+platforms = [
+    Rect((150, HEIGHT-100), (120, 20)),  # Plataforma média
+    Rect((300, HEIGHT-150), (120, 20)),  # Plataforma média
+    Rect((450, HEIGHT-200), (120, 20)),  # Plataforma alta
+    Rect((600, HEIGHT-250), (120, 20)),  # Plataforma alta
+    Rect((750, HEIGHT-115), (120, 20)),  # Plataforma alta
+]
 
+def reset_enemies():
+    global enemies
+    enemies = []
+    for enemy_data in INITIAL_ENEMIES:
+        enemy = Actor('enemy', pos=enemy_data['pos'])
+        enemy.direction = -1
+        enemy.start_x = enemy.x
+        enemy.patrol_distance = 50
+        enemies.append(enemy)    
+
+enemies = []
+reset_enemies()
+
+for enemy in enemies:
+    enemy.direction = -1
+    enemy.start_x = enemy.x  # Posição inicial
+    enemy.patrol_distance = 50  # Distância de patrulha
 
 background = 'sample'
 
-
-
 def update():
-    # Garante que a música sempre esteja tocando
+    global GAME_WON
+    
     if not music.is_playing('theme'):
         music.play('theme')
         music.set_volume(0.5)
 
+
     if GAME_ACTIVE:
 
-        if GAME_ACTIVE:
-            player.is_jumping = not player.on_ground
+
+        if player.colliderect(brother):
+            GAME_WON = True
+            sounds.win.play()
+            sounds.win.set_volume(0.5)
+
+        old_x = player.x
+        old_y = player.y
+        old_bottom = player.bottom
+
+        player.vy += GRAVITY  # Aplica gravidade
+        player.y += player.vy 
+        
+        player.on_ground = False
+        for platform in platforms:
+            if player.colliderect(platform):
+                # Calcula as bordas de colisão
+                overlap_left = player.right - platform.left
+                overlap_right = platform.right - player.left
+                overlap_top = player.bottom - platform.top
+                overlap_bottom = platform.bottom - player.top
+
+                # Encontra a menor sobreposição
+                min_overlap = min(overlap_left, overlap_right, overlap_top, overlap_bottom)
+
+                # Resolve a colisão pela menor distância
+                if min_overlap == overlap_top and player.vy > 0:
+                    # Colisão por cima da plataforma
+                    player.bottom = platform.top
+                    player.vy = 0
+                    player.on_ground = True
+                    player.is_jumping = False
+                elif min_overlap == overlap_bottom and player.vy < 0:
+                    # Colisão por baixo da plataforma
+                    player.top = platform.bottom
+                    player.vy = 0
+                elif min_overlap == overlap_left:
+                    # Colisão pela esquerda
+                    player.right = platform.left
+                elif min_overlap == overlap_right:
+                    # Colisão pela direita
+                    player.left = platform.right
+
+        for enemy in enemies: 
+            enemy.x += ENEMY_SPEED * enemy.direction           
+
+            if enemy.x > enemy.start_x + enemy.patrol_distance:
+                enemy.direction = -1
+            elif enemy.x < enemy.start_x - enemy.patrol_distance:
+                enemy.direction = 1
+
+            if player.colliderect(enemy):
+                    # Verifica se o player está caindo e acima do inimigo
+                if player.vy > 0 and player.bottom < enemy.top + 10:
+                        enemies.remove(enemy)
+                        player.vy = JUMP_SPEED * 0.7
+                        
+                else:
+                        player.pos = (0, HEIGHT-50)
+                        player.vy = 0
+                        reset_enemies()
+
 
         if keyboard.left:
             player.x -= MOVE_SPEED
             player.facing_right = False
             update_player_animation('walk')
+          
         elif keyboard.right:
             player.x += MOVE_SPEED
             player.facing_right = True
             update_player_animation('walk')
+        
+        elif player.is_jumping:
+            update_player_animation('jump')   
+         
         else:
             update_player_animation('idle')
-
-        # Atualiza animação de pulo
-        if player.is_jumping:
-            update_player_animation('jump')
-        
-        player.vy += GRAVITY  # Aplica gravidade
-        player.y += player.vy 
-
-        if player.bottom > HEIGHT:
-            player.bottom = HEIGHT
+     
+        if player.bottom > HEIGHT - 30:
+            player.bottom = HEIGHT - 30
             player.vy = 0
             player.on_ground = True
-        else:
-            player.on_ground = False
+            player.is_jumping = False
 
-        if player.left < 0:
-            player.left = 0
-        if player.right > WIDTH:
-            player.right = WIDTH
-
+      
 
         if player.left < 0:
             player.left = 0
@@ -100,8 +190,8 @@ def update():
         if player.top < 0:
             player.top = 0
         if player.bottom > HEIGHT:
-            player.bottom = HEIGHT    
-
+            player.bottom = HEIGHT  
+          
 def draw_menu():
     screen.clear()
     screen.blit(background, (0, 0))
@@ -128,6 +218,13 @@ def draw_menu():
 def draw_game():
     screen.clear()  
     screen.blit(background, (0, 0))
+    floor_rect = Rect((0, HEIGHT - 30 ), (WIDTH, 60))
+    screen.draw.filled_rect(floor_rect, FLOOR_COLOR)
+    for platform in platforms:
+        screen.blit(PLATFORM_IMAGE, platform)
+    for enemy in enemies:
+        enemy.draw()
+    brother.draw()
     player.draw()
     music_color = MUSIC_HOVER if music_hover else MUSIC_COLOR
     screen.draw.filled_circle(music_button_pos, MUSIC_BUTTON_RADIUS, music_color)
@@ -137,11 +234,19 @@ def draw_game():
     else:
         screen.draw.text("Unmute", center=music_button_pos, fontsize=20, color="white")
 
+    if GAME_WON:
+        screen.draw.text("YOU SAVED YOUR BROTHER!", 
+                        center=(WIDTH/2, HEIGHT/2), 
+                        fontsize=60, 
+                        color="yellow")
+
 def draw():
     if GAME_ACTIVE:
         draw_game()
     else:
         draw_menu()
+
+
 
 def on_mouse_move(pos):
     global start_hover, exit_hover, music_hover
@@ -176,14 +281,16 @@ def on_mouse_down(pos):
         if music_playing:
             music.set_volume(1)
             sounds.hover.set_volume(1)
+       
         else:
             music.set_volume(0)
             sounds.hover.set_volume(0)
+     
 
     if not GAME_ACTIVE:
         if start_button_pos.collidepoint(pos):
             GAME_ACTIVE = True
-            player.pos = (WIDTH//2, HEIGHT//2)
+            player.pos = (0, HEIGHT)
         
         if exit_button_pos.collidepoint(pos):
             exit()
@@ -192,23 +299,32 @@ def on_key_down(key):
     if GAME_ACTIVE and key == keys.SPACE and player.on_ground:
         player.vy = JUMP_SPEED
         player.on_ground = False    
-            
+        player.is_jumping = True  
+        sounds.hover.play()  
+        player.image = 'player_jump_right' if player.facing_right else 'player_jump_left'
+                  
 def update_player_animation(state):
+
      # Atualiza o tempo da animação
     player.animation_time += ANIMATION_SPEED
     
     # Define os frames baseado no estado
     if state == 'idle':
-        frames = ['player_idle_right', 'player_sprite'] if player.facing_right else ['player_sprite', 'player_idle_left2']
+        frames = ['player_idle_right', 'player_idle_right2'] if player.facing_right else ['player_idle_left', 'player_idle_left2']
     elif state == 'walk':
-        frames = ['player_walk_right2', 'player_walk_right1'] if player.facing_right else ['player_walk_left1', 'player_walk_left2']
+        frames = ['player_walk_right1', 'player_walk_right2'] if player.facing_right else ['player_walk_left1', 'player_walk_left2']
     elif state == 'jump':
         frames = ['player_jump_right'] if player.facing_right else ['player_jump_left']
+
     
+
+    if player.current_frame >= len(frames):
+        player.current_frame = 0
     # Atualiza o frame atual
-    if player.animation_time >= 1:
+    if player.animation_time >=1:
         player.animation_time = 0
         player.current_frame = (player.current_frame + 1) % len(frames)
     
     # Aplica o sprite atual
     player.image = frames[player.current_frame]
+
